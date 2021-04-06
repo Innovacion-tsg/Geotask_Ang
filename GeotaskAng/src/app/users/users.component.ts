db16a5da8dafd8aa85b03b0217ba8436a926dc95
Innovacion-tsg <innovacion@tsg.net.co>
2021-04-06 17:01:39 -0500

    Creacion de usuario

--- GeotaskAng/src/app/users/users.component.ts
+++ GeotaskAng/src/app/users/users.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit ,AfterViewInit, ViewChild} from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import{ GlobalConstants } from '../global-constants';
+import { NgForm, FormGroup, FormBuilder, Validators } from '@angular/forms';
+interface companiesObj{
+  idcompany: Int16Array;
+  company: String;
+  idcompany_type: Int16Array;
+  is_active: Boolean;
+}
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
+
+
 export class UsersComponent implements OnInit {
+  
+
   options = [
     {
       name: "Crear",
@@ -61,16 +72,25 @@ export class UsersComponent implements OnInit {
       ]
     }
   ];
-  users;  
+  users;
+  roles;
+  companies: companiesObj[] = [];
+  eps: companiesObj[] = [];
+  arl: companiesObj[] = [];
+  it;
+  specialties;
+  selectedValue: string;
   isLoading=false;
-  displayedColumns: string[] = ['iduser','first_name','last_name','email','idrole','button'];
+  displayedColumns: string[] = ['iduser','first_name','last_name','email','role','is_active','button'];
   dataSource;
   formCreate=false;
   formEdit=false;
+  formDelete=false;
+  form: FormGroup;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,private formBuilder: FormBuilder) {
   
   }
 
@@ -81,15 +101,73 @@ export class UsersComponent implements OnInit {
         'Content-Type': 'application/json'
       })
     }).subscribe(response => {
-      //this.users=users;
       this.users=response;
       this.dataSource=new MatTableDataSource(this.users);
       this.dataSource.paginator = this.paginator;
-      this.isLoading = false;
     });
+    this.http.get('http://'+GlobalConstants.report_server_address+'/roles', {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }).subscribe(response => {
+      this.roles=response;
+    });
+    let companies;
+    this.http.get('http://'+GlobalConstants.report_server_address+'/companies', {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }).subscribe(response => {
+      companies=response;
+      for(let obj of companies){
+        let a=obj;
+        switch(obj.idcompany_type){
+          case(1):
+            this.companies.push(obj);
+            break;
+          case(2):
+            this.eps.push(obj);
+            break;
+          case(3):
+            this.arl.push(obj);
+            break;
+        }
+      }
+    });
+    this.http.get('http://'+GlobalConstants.report_server_address+'/identifications_types', {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }).subscribe(response => {
+      this.it=response;
+    });
+    this.http.get('http://'+GlobalConstants.report_server_address+'/specialties', {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }).subscribe(response => {
+      this.specialties=response;
+    });
+    this.buildForm();
+    this.isLoading=false;
   }
 
-
+  buildForm(){
+    this.form = this.formBuilder.group({
+      first_name: [null, [Validators.required]],
+      last_name: [null, [Validators.required]],
+      email: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      idrole: [null, [Validators.required]],
+      ididentification_type: [null, null],
+      identification: [null, null],
+      phone: [null, null],
+      idspecialty:[null, null],
+      idcompany:[null, null],
+      ideps:[null, null],
+      idarl:[null, null]
+    });
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -103,23 +181,48 @@ export class UsersComponent implements OnInit {
   DeleteUser(user): void{
     let u=user;
     const index: number = this.users.indexOf(u);
+    let delet;
     if(index !== -1){
-      this.users.splice(index,1);
+      delet=this.users[index];
+    }
+    this.http.get('http://'+GlobalConstants.report_server_address+'/users/delete', {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'})
+      ,
+      params: new HttpParams().set("id",delet.iduser)
+    }).subscribe(response => {
+      debugger;
+      let u=response;
+      this.users[index].is_active=false;
+      this.dataSource=new MatTableDataSource(this.users);
+      this.dataSource.paginator = this.paginator;
+      this.isLoading = false;
+    });
+  }
+
+  toggleCreate(){
+    if (this.formCreate) {
+      this.formCreate = false;
+    } else {
+      this.formCreate = true;
     }
   }
 
-  formVisible(type){
-    switch(type){
-      case "1":
-        this.formCreate=!this.formCreate;
-        break;
-      case "2":
-        this.formEdit=!this.formEdit;
-        break;
-      default:
-        break;
-    }
-    if(type==="create"){
+  onSubmit(event) {
+    event.preventDefault();
+    debugger;
+    let info=this.form.value;
+    info.idrole=parseInt(info.idrole);
+    if (this.form.valid) {
+      this.http.post('http://'+GlobalConstants.report_server_address+'/users', info,{
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        }),
+        responseType: 'text' as 'json'
+      }).subscribe(response => {
+        debugger;
+        let a=response;
+      });
     }
   }
 }
