a20b191be96b90204ccd495ce6d17e3f4cb1e3cf
Innovacion-tsg <innovacion@tsg.net.co>
2021-04-14 17:22:52 -0500

    Finalizada interfaz usuario

--- GeotaskAng/src/app/users/users.component.ts
+++ GeotaskAng/src/app/users/users.component.ts
@@ -1,26 +1,15 @@
 import { Component, OnInit ,AfterViewInit, ViewChild} from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import{ GlobalConstants } from '../global-constants';
-import { NgForm, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NgForm, FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 interface companiesObj{
   idcompany: Int16Array;
   company: String;
   idcompany_type: Int16Array;
-  is_active: Boolean;
-}
-
-interface UserObject{
-  first_name : String;
-  password:String;
-  role:String;
-  iduser:Int16Array;
-  email:String;
-  password_hash:String;
-  password_salt:String;
-  last_name: String;
-  idrole: Int16Array;
   is_active: Boolean;
 }
 
@@ -33,58 +22,6 @@ interface UserObject{
 
 export class UsersComponent implements OnInit {
   
-
-  options = [
-    {
-      name: "Crear",
-      description: "Crear usuario o verificar si ya existe en la plataforma",
-      options: [
-        {
-          name: "Crear",
-          icono: "add_circle"
-        },{
-          name: "Verificar",
-          icono: "verified"
-        }
-      ]
-    },{
-      name: "Editar",
-      description: "Editar un usuario",
-      options: [
-        {
-          name: "Editar",
-          icono: "edit"
-        }
-      ]
-    },{
-      name: "Eliminar",
-      description: "Eliminar un usuario o ver historial de eliminaciones",
-      options: [
-        {
-          name: "Eliminar",
-          icono: "person_remove"
-        },{
-          name: "Historial",
-          icono: "history"
-        }
-      ]
-    },{
-      name: "Cuadrilla",
-      description: "Crear cuadrilla, modificarla y verificarla",
-      options: [
-        {
-          name: "Crear",
-          icono: "group_add"
-        },{
-          name: "Modificar",
-          icono: "engineering"
-        },{
-          name: "Verificar",
-          icono: "admin_panel_settings"
-        }
-      ]
-    }
-  ];
   users;
   roles;
   companies: companiesObj[] = [];
@@ -93,16 +30,17 @@ export class UsersComponent implements OnInit {
   it;
   specialties;
   selectedValue: string;
+  element;
   isLoading=false;
   displayedColumns: string[] = ['iduser','first_name','last_name','email','role','is_active','button'];
   dataSource;
   formCreate=false;
-  formEdit=false;
+  isAdd=true;
   formDelete=false;
   form: FormGroup;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  
+  @ViewChild(MatSort) sort: MatSort;
   constructor(private http: HttpClient,private formBuilder: FormBuilder) {
   
   }
@@ -116,8 +54,7 @@ export class UsersComponent implements OnInit {
       })
     }).subscribe(response => {
       this.users=response;
-      this.dataSource=new MatTableDataSource(this.users);
-      this.dataSource.paginator = this.paginator;
+      this.UpdateTable();
     });
     this.http.get('http://'+GlobalConstants.report_server_address+'/roles', {
       headers: new HttpHeaders({
@@ -163,15 +100,67 @@ export class UsersComponent implements OnInit {
       this.specialties=response;
     });
     this.buildForm();
+    this.setRoleValidators();
     this.isLoading=false;
   }
+
+  UpdateTable(){
+    this.dataSource=new MatTableDataSource(this.users);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+
+  setRoleValidators(){
+    const ididentification_typeControl= this.form.get('ididentification_type');
+    const identificationControl= this.form.get('identification');
+    const phoneControl=this.form.get('phone');
+    const idspecialtyControl=this.form.get('idspecialty');
+    const idcompanyControl=this.form.get('idcompany');
+    const idepsControl=this.form.get('ideps');
+    const idarlControl=this.form.get('idarl');
+    const rhControl=this.form.get('rh');
+    this.form.get('idrole').valueChanges.subscribe(idrole =>{
+      let idrole2=parseInt(idrole);
+      if(idrole2 == 3){
+        ididentification_typeControl.setValidators([Validators.required]);
+        identificationControl.setValidators([Validators.required]);
+        phoneControl.setValidators([Validators.required]);
+        idspecialtyControl.setValidators([Validators.required]);
+        idcompanyControl.setValidators([Validators.required]);
+        idepsControl.setValidators([Validators.required]);
+        idarlControl.setValidators([Validators.required]);
+        rhControl.setValidators([Validators.required]);
+      }else{
+        ididentification_typeControl.setValidators(null);
+        identificationControl.setValidators(null);
+        phoneControl.setValidators(null);
+        idspecialtyControl.setValidators(null);
+        idcompanyControl.setValidators(null);
+        idepsControl.setValidators(null);
+        idarlControl.setValidators(null);
+        rhControl.setValidators(null);
+      }
+
+      ididentification_typeControl.updateValueAndValidity();
+      identificationControl.updateValueAndValidity();
+      phoneControl.updateValueAndValidity();
+      idspecialtyControl.updateValueAndValidity();
+      idcompanyControl.updateValueAndValidity();
+      idepsControl.updateValueAndValidity();
+      idarlControl.updateValueAndValidity();
+      rhControl.updateValueAndValidity();
+    })
+  }
+
+
 
   buildForm(){
     this.form = this.formBuilder.group({
       first_name: [null, [Validators.required]],
       last_name: [null, [Validators.required]],
-      email: [null, [Validators.required]],
-      password: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [this.isAdd ? Validators.required: Validators.nullValidator]],
       idrole: [null, [Validators.required]],
       ididentification_type: [null, null],
       identification: [null, null],
@@ -189,13 +178,60 @@ export class UsersComponent implements OnInit {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  EditUser(id){
-
+  EditUser(element){
+    if(this.formCreate){
+      this.toggleCreate();
+    }
+    this.toggleCreate();
+    this.isAdd=false;
+    this.element=element;
+    this.selectedValue=element.idrole.toString();
+    if(element.idrole==3){
+      this.http.get('http://'+GlobalConstants.report_server_address+'/users_info/GetById', {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'})
+      ,
+      params: new HttpParams().set("id",element.iduser)
+      }).subscribe(response => {
+        let obj: any = response;
+        this.form.setValue({
+          first_name : element.first_name,
+          last_name : element.last_name,
+          email : element.email,
+          password : 'JIONEKDIOELFGJAH',
+          idrole : element.idrole.toString(),
+          ididentification_type: obj.ididentification_type.toString(),
+          identification: obj.identification,
+          phone: obj.phone,
+          idspecialty: obj.idspecialty.toString(),
+          idcompany:obj.idcompany.toString(),
+          ideps:obj.ideps.toString(),
+          idarl:obj.idarl.toString(),
+          rh: obj.rh
+        });
+      });
+    }else{
+      this.form.setValue({
+        first_name : element.first_name,
+        last_name : element.last_name,
+        email : element.email,
+        password : 'JIONEKDIOELFGJAH',
+        idrole : element.idrole.toString(),
+        ididentification_type: null,
+        identification: null,
+        phone: null,
+        idspecialty: null,
+        idcompany:null,
+        ideps:null,
+        idarl:null,
+        rh: ''
+      });
+    }
   }
 
   DeleteUser(user): void{
     let u=user;
-    const index: number = this.users.indexOf(u);
+    let index: number = this.users.indexOf(u);
     let delet;
     if(index !== -1){
       delet=this.users[index];
@@ -206,11 +242,9 @@ export class UsersComponent implements OnInit {
       ,
       params: new HttpParams().set("id",delet.iduser)
     }).subscribe(response => {
-      debugger;
       let u=response;
       this.users[index].is_active=false;
-      this.dataSource=new MatTableDataSource(this.users);
-      this.dataSource.paginator = this.paginator;
+      this.UpdateTable();
       this.isLoading = false;
     });
   }
@@ -218,54 +252,96 @@ export class UsersComponent implements OnInit {
   toggleCreate(){
     if (this.formCreate) {
       this.formCreate = false;
+      this.isAdd = false;
     } else {
       this.formCreate = true;
+      this.isAdd= true;
+      this.element=null;
     }
+    this.clearMe();
+  }
+
+  validateForm(){
+
   }
 
   onSubmit(event) {
     event.preventDefault();
     let info=this.form.value;
     info.idrole=parseInt(info.idrole);
-    
-    if (this.form.valid) {
-      this.http.post('http://'+GlobalConstants.report_server_address+'/users', info,{
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json'
-        }),
-        responseType: 'json'
-      }).subscribe(response => {
-        debugger;
-        let value : any = response;
-        value.password_hash="";
-        value.password_salt="";
-        value.password="";
-        this.users.push(value);
-        this.dataSource=new MatTableDataSource(this.users);
-        this.dataSource.paginator = this.paginator;
-        if(info.idrole === 3){
-          info.iduser = value.iduser;
-          info.phone = info.phone.toString();
-          info.identification = info.identification.toString();
-          info.ididentification_type = parseInt(info.ididentification_type);
-          info.idspecialty = parseInt(info.idspecialty);
-          info.idcompany= parseInt(info.idcompany);
-          info.idarl = parseInt(info.idarl);
-          info.ideps = parseInt(info.ideps);
-          this.http.post('http://'+GlobalConstants.report_server_address+'/users_info', info,{
+    if(this.form.valid){
+      if (this.isAdd) {
+        info['is_active']=true;
+        this.http.post('http://'+GlobalConstants.report_server_address+'/users', info,{
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+          }),
+          responseType: 'json'
+        }).subscribe(response => {
+          let value : any = response;
+          value.password_hash="";
+          value.password_salt="";
+          value.password="";
+          this.users.push(value);
+          this.UpdateTable();
+          if(info.idrole === 3){
+            info.iduser = value.iduser;
+            info.phone = info.phone.toString();
+            info.identification = info.identification.toString();
+            info.ididentification_type = parseInt(info.ididentification_type);
+            info.idspecialty = parseInt(info.idspecialty);
+            info.idcompany= parseInt(info.idcompany);
+            info.idarl = parseInt(info.idarl);
+            info.ideps = parseInt(info.ideps);
+            this.http.post('http://'+GlobalConstants.report_server_address+'/users_info', info,{
+            headers: new HttpHeaders({
+              'Content-Type': 'application/json'
+            }),
+            responseType: 'text' as 'json'
+            }).subscribe(response => {
+              let a = response;
+            });
+          }
+          this.clearMe();
+        });
+      }else{
+        info.is_active=this.element.is_active;
+        let u=this.findUser(this.element.iduser);
+        let index: number = this.users.indexOf(u);
+        info['iduser']=this.element.iduser;
+        this.http.put('http://'+GlobalConstants.report_server_address+'/users', info,{
           headers: new HttpHeaders({
             'Content-Type': 'application/json'
           }),
           responseType: 'text' as 'json'
           }).subscribe(response => {
-            debugger;
-            let a = response;
+            this.users[index].first_name=info.first_name;
+            this.users[index].last_name=info.last_name;
+            this.users[index].idrole=info.idrole.toString();
+            this.UpdateTable();
+            if(info.idrole === 3){
+              info.phone = info.phone.toString();
+              info.identification = info.identification.toString();
+              info.ididentification_type = parseInt(info.ididentification_type);
+              info.idspecialty = parseInt(info.idspecialty);
+              info.idcompany= parseInt(info.idcompany);
+              info.idarl = parseInt(info.idarl);
+              info.ideps = parseInt(info.ideps);
+              this.http.put('http://'+GlobalConstants.report_server_address+'/users_info', info,{
+              headers: new HttpHeaders({
+                'Content-Type': 'application/json'
+              }),
+              responseType: 'text' as 'json'
+              }).subscribe(response => {
+                let a = response;
+              });
+            }
           });
-        }
         this.clearMe();
-      });
+      }
     }
   }
+
 
 
   clearMe(): void {
@@ -275,7 +351,15 @@ export class UsersComponent implements OnInit {
     });
   }
 
-
+  findUser(iduser): any{
+    let a=null;
+    this.users.forEach(element => {
+      if(element.iduser == iduser){
+        a=element;
+      }
+    });
+    return a;
+  }
 }
 
 
